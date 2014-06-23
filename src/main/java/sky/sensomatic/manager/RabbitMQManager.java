822c87fb27cd29a49df4992c514c4fdab2259547
synapticloop <contact@synapticloop.com>
2014-06-23 19:16:10 +0100

    refactored rabbitmq manager

--- src/main/java/sky/sensomatic/manager/RabbitMQManager.java
+++ src/main/java/sky/sensomatic/manager/RabbitMQManager.java
@@ -14,12 +14,24 @@ public class RabbitMQManager {
 	private static String password = null;
 	private static String queueName = null;
 
-	public RabbitMQManager(String host, String virtualHost, String queueName, String username, String password) {
-		RabbitMQManager.host = host;
-		RabbitMQManager.virtualHost = virtualHost;
-		RabbitMQManager.username = username;
-		RabbitMQManager.password = password;
-		RabbitMQManager.queueName = queueName;
+	private static String managerHost = null;
+	private static String managerVirtualHost = null;
+	private static String managerUsername = null;
+	private static String managerPassword = null;
+	private static String managerQueueName = null;
+
+	public static void initialise() {
+		RabbitMQManager.host = PropertyManager.getRabbitMQHost();
+		RabbitMQManager.virtualHost = PropertyManager.getRabbitMQVHost();
+		RabbitMQManager.username = PropertyManager.getRabbitMQUsername();
+		RabbitMQManager.password = PropertyManager.getRabbitMQPassword();
+		RabbitMQManager.queueName = PropertyManager.getRabbitMQQueueName();
+
+		RabbitMQManager.managerHost = PropertyManager.getRabbitMQManagerHost();
+		RabbitMQManager.managerVirtualHost = PropertyManager.getRabbitMQManagerVHost();
+		RabbitMQManager.managerUsername = PropertyManager.getRabbitMQManagerUsername();
+		RabbitMQManager.managerPassword = PropertyManager.getRabbitMQManagerPassword();
+		RabbitMQManager.managerQueueName = PropertyManager.getRabbitMQManagerQueueName();
 	}
 
 	public static ConnectionFactory getConnectionFactory() {
@@ -28,6 +40,15 @@ public class RabbitMQManager {
 		factory.setVirtualHost(virtualHost);
 		factory.setUsername(username);
 		factory.setPassword(password);
+		return(factory);
+	}
+
+	public static ConnectionFactory getManagerConnectionFactory() {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(managerHost);
+		factory.setVirtualHost(managerVirtualHost);
+		factory.setUsername(managerUsername);
+		factory.setPassword(managerPassword);
 		return(factory);
 	}
 
@@ -54,7 +75,34 @@ public class RabbitMQManager {
 		return(true);
 	}
 
+	public static boolean pushManagerMessage(String payload) {
+		// pump the payload onto the manager rabbit MQ
+		ConnectionFactory factory = getManagerConnectionFactory();
+
+		Connection connection = null;
+		Channel channel = null;
+		try {
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+			channel.queueDeclare(managerQueueName, false, false, false, null);
+			String message = payload;
+			channel.basicPublish("", managerQueueName, null, message.getBytes());
+		} catch (IOException ioex) {
+			return(false);
+		} finally {
+			if(null != connection) {
+				try { channel.close(); } catch (IOException ioex) { channel = null; }
+				try { connection.close(); } catch (IOException ioex) { connection = null; }
+			}
+		}
+		return(true);
+	}
+
 	public static String getQueueName() {
 		return(queueName);
+	}
+
+	public static String getManagerQueueName() {
+		return(managerQueueName);
 	}
 }
