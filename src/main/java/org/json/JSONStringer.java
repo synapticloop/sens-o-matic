189b26da8996ec0dc1c4f6ef8d379bc6195e9a62
synapticloop <contact@synapticloop.com>
2014-06-23 19:13:50 +0100

    code cleanup of JSON

--- src/main/java/org/json/JSONStringer.java
+++ src/main/java/org/json/JSONStringer.java
@@ -22,7 +22,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-*/
+ */
 
 import java.io.StringWriter;
 
@@ -57,22 +57,22 @@ import java.io.StringWriter;
  * @version 2008-09-18
  */
 public class JSONStringer extends JSONWriter {
-    /**
-     * Make a fresh JSONStringer. It can be used to build one JSON text.
-     */
-    public JSONStringer() {
-        super(new StringWriter());
-    }
+	/**
+	 * Make a fresh JSONStringer. It can be used to build one JSON text.
+	 */
+	public JSONStringer() {
+		super(new StringWriter());
+	}
 
-    /**
-     * Return the JSON text. This method is used to obtain the product of the
-     * JSONStringer instance. It will return <code>null</code> if there was a
-     * problem in the construction of the JSON text (such as the calls to
-     * <code>array</code> were not properly balanced with calls to
-     * <code>endArray</code>).
-     * @return The JSON text.
-     */
-    public String toString() {
-        return this.mode == 'd' ? this.writer.toString() : null;
-    }
+	/**
+	 * Return the JSON text. This method is used to obtain the product of the
+	 * JSONStringer instance. It will return <code>null</code> if there was a
+	 * problem in the construction of the JSON text (such as the calls to
+	 * <code>array</code> were not properly balanced with calls to
+	 * <code>endArray</code>).
+	 * @return The JSON text.
+	 */
+	public String toString() {
+		return this.mode == 'd' ? this.writer.toString() : null;
+	}
 }
--- src/main/java/org/json/Property.java
+++ src/main/java/org/json/Property.java
@@ -22,7 +22,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-*/
+ */
 
 import java.util.Enumeration;
 import java.util.Iterator;
@@ -34,39 +34,40 @@ import java.util.Properties;
  * @version 2014-05-03
  */
 public class Property {
-    /**
-     * Converts a property file object into a JSONObject. The property file object is a table of name value pairs.
-     * @param properties java.util.Properties
-     * @return JSONObject
-     * @throws JSONException
-     */
-    public static JSONObject toJSONObject(java.util.Properties properties) throws JSONException {
-        JSONObject jo = new JSONObject();
-        if (properties != null && !properties.isEmpty()) {
-            Enumeration enumProperties = properties.propertyNames();
-            while(enumProperties.hasMoreElements()) {
-                String name = (String)enumProperties.nextElement();
-                jo.put(name, properties.getProperty(name));
-            }
-        }
-        return jo;
-    }
+	/**
+	 * Converts a property file object into a JSONObject. The property file object is a table of name value pairs.
+	 * @param properties java.util.Properties
+	 * @return JSONObject
+	 * @throws JSONException
+	 */
+	@SuppressWarnings("rawtypes")
+	public static JSONObject toJSONObject(java.util.Properties properties) throws JSONException {
+		JSONObject jo = new JSONObject();
+		if (properties != null && !properties.isEmpty()) {
+			Enumeration enumProperties = properties.propertyNames();
+			while(enumProperties.hasMoreElements()) {
+				String name = (String)enumProperties.nextElement();
+				jo.put(name, properties.getProperty(name));
+			}
+		}
+		return jo;
+	}
 
-    /**
-     * Converts the JSONObject into a property file object.
-     * @param jo JSONObject
-     * @return java.util.Properties
-     * @throws JSONException
-     */
-    public static Properties toProperties(JSONObject jo)  throws JSONException {
-        Properties  properties = new Properties();
-        if (jo != null) {
-            Iterator<String> keys = jo.keys();
-            while (keys.hasNext()) {
-                String name = keys.next();
-                properties.put(name, jo.getString(name));
-            }
-        }
-        return properties;
-    }
+	/**
+	 * Converts the JSONObject into a property file object.
+	 * @param jo JSONObject
+	 * @return java.util.Properties
+	 * @throws JSONException
+	 */
+	public static Properties toProperties(JSONObject jo)  throws JSONException {
+		Properties  properties = new Properties();
+		if (jo != null) {
+			Iterator<String> keys = jo.keys();
+			while (keys.hasNext()) {
+				String name = keys.next();
+				properties.put(name, jo.getString(name));
+			}
+		}
+		return properties;
+	}
 }
