4b283b41e8ad0b35da7418e2106535d9caff2bc1
synapticloop <contact@synapticloop.com>
2014-06-30 20:46:44 +0100

    new dependencies
    also split out distributable jar into two components

---
+++ prepare-for-copy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+rm -rf ./build-eclipse
+rm -rf ./build
+rm -rf ./dist
+
+rm -rf ../sens-o-matic-manager/build-eclipse
+rm -rf ../sens-o-matic-manager/build
+rm -rf ../sens-o-matic-manager/dist
+
+rm -rf ../sens-o-matic-dashboard/build-eclipse
+rm -rf ../sens-o-matic-dashboard/build
+rm -rf ../sens-o-matic-dashboard/dist
+
+rm -rf ../sens-o-matic-demo/build-eclipse
+rm -rf ../sens-o-matic-demo/build
+rm -rf ../sens-o-matic-demo/dist
+
+rm -rf ../sens-o-matic-rabbit-api/build-eclipse
+rm -rf ../sens-o-matic-rabbit-api/build
+rm -rf ../sens-o-matic-rabbit-api/dist
+
+rm -rf ../sens-o-matic-loader/build-eclipse
+rm -rf ../sens-o-matic-loader/build
+rm -rf ../sens-o-matic-loader/dist
+
+rm -rf ../sens-o-matic-picker/build-eclipse
+rm -rf ../sens-o-matic-picker/build
+rm -rf ../sens-o-matic-picker/dist
